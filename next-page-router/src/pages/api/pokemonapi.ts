86e5c6d4fdfadd054211bd0640e33459e479e1d6
wwliurevou <widyawinataliu@gmail.com>
2024-05-23 23:27:58 +0800

    removed unnecessary page

--- next-page-router/src/pages/api/pokemonapi.ts
+++ next-page-router/src/pages/api/pokemonapi.ts
@@ -2,5 +2,9 @@ export async function getStaticProps() {
   const res = await fetch("https://pokeapi.co/api/v2/pokemon");
   const pokemons = await res.json();
 
-  return pokemons.results;
+  return {
+    props: {
+      pokemons
+    },
+  };
 }
